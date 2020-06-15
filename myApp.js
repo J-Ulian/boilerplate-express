9ae48fb07acbcb57ee67a227dcdb687df60a353a
J Ulian <hudatov@gmail.com>
2020-06-15 19:38:19 +0300

    all routes

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
-
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
 // --> 7)  Mount the Logger middleware here
 
@@ -24,8 +24,8 @@ var app = express();
 
 
 /** 6) Use the .env file to configure the app */
- 
- 
+
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
@@ -39,7 +39,7 @@ var app = express();
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -49,9 +49,85 @@ var app = express();
 
 
 // This would be part of the basic setup of an Express app
-// but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
+// but to allow FCC to run tests, the server is already active/**
+app.listen(process.env.PORT || 80);
+
+console.log("Hello World")
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
+
+
+//app.get("/", (req, res) => {
+//    res.send("Hello Express");
+//});
+
+absolutePath = __dirname + /views/index.html;
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === uppercase) {
+        response = "Hello World".toUpperCase();
+        res.json({
+            "message": response
+        })
+    } else {
+        res.json({
+            "message": "Hello json"
+        })
+    }
+})
+
+
+app.use(function middleware(req, res, next) {
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string)
+    next();
+});
+
+
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.send({
+        time: req.time
+    });
+});
+
+app.get("/:word/echo", (req, res) => {
+    const {
+        word
+    } = req.params;
+    res.json({
+        echo: word
+    })
+})
+
+app.get("/name", (req, res) => {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    res.json({
+        name: `${firstName} ${lastName}`
+    })
+})
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.post("/name", function (req, res) {
+    // Handle the data in the request
+    var string = req.body.first + " " + req.body.last;
+    res.json({
+        name: string
+    });
+});
